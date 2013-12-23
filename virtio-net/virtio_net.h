24b9446e531548c4c3a0b3e35e7152afca4f2326
Phil Jordan <phil@philjordan.eu>
2013-12-22 22:53:59 -0800

    License change: now using LGPLv3.

--- virtio-net/virtio_net.h
+++ virtio-net/virtio_net.h
@@ -1,72 +1,22 @@
-/* Copyright 2011 Phil Jordan <phil@philjordan.eu>
- * Triple-licensed under the zLib license, the 3-clause BSD License and the MIT License */
-
-/*
-	This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any damages
-  arising from the use of this software.
-
-  Permission is granted to anyone to use this software for any purpose,
-  including commercial applications, and to alter it and redistribute it
-  freely, subject to the following restrictions:
-
-  1. The origin of this software must not be misrepresented; you must not
-     claim that you wrote the original software. If you use this software
-     in a product, an acknowledgment in the product documentation would be
-     appreciated but is not required.
-  2. Altered source versions must be plainly marked as such, and must not be
-     misrepresented as being the original software.
-  3. This notice may not be removed or altered from any source distribution.
-
-AND/OR:
-
-Copyright (c) 2011 Phil Jordan <phil@philjordan.eu>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-AND/OR:
-
-Copyright (c) 2011 Phil Jordan <phil@philjordan.eu>
- All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this list
-of conditions and the following disclaimer.
-Redistributions in binary form must reproduce the above copyright notice, this
-list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-The name of Phil Jordan may NOT be used to endorse or promote products derived
-from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-*/
+/* Copyright 2011, 2013 Phil Jordan <phil@philjordan.eu>
+ * This code made available under the GNU LGPL
+ * (GNU Library/Lesser General Public License)
+ * A copy of this license can be found in the LICENSE file provided together
+ * with this source file, or at https://www.gnu.org/licenses/lgpl.html if
+ * that file is missing.
+ *
+ * For practical purposes, what does this mean?
+ * - You may use, modify and/or run this code freely
+ * - You may redistribute this code and the compiled kext, provided that
+ *   copyright notices remain intact and that you make available the full source
+ *   code, including any modifications.
+ * - You may create and distribute other kexts with different licenses which
+ *   depend upon this kext, as long as this kext remains independently loadable
+ *   and modifiable by all users.
+ *
+ * If you require additional permissions not covered by this license, please
+ * contact the author at phil@philjordan.eu - other licensing options are available.
+ */
 
 #ifndef VIRTIO_NET_H
 #define VIRTIO_NET_H
